15a44178cf02eb4a4c07a4484fb1be7334744aac
SUBHAVA OJHA <subhava.22gcebcs190@galgotiacollege.edu>
2025-09-10 00:45:55 +0530

    feat: email service using email.js

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,6 +7,10 @@ import { EarthCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { slideIn } from "../utils/motion";
 
+//yjLMjud_z9tPFqycJ
+//template_qwz9fii
+//service_ld0ib9b
+
 const Contact = () => {
   const formRef = useRef();
   const [form, setForm] = useState({
@@ -18,8 +22,7 @@ const Contact = () => {
   const [loading, setLoading] = useState(false);
 
   const handleChange = (e) => {
-    const { target } = e;
-    const { name, value } = target;
+    const { name, value } = e.target;
 
     setForm({
       ...form,
@@ -31,18 +34,24 @@ const Contact = () => {
     e.preventDefault();
     setLoading(true);
 
+    //yjLMjud_z9tPFqycJ
+//template_qwz9fii
+//service_ld0ib9b
+
     emailjs
       .send(
-        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        'service_ld0ib9b',
+        'template_qwz9fii',
+        
         {
           from_name: form.name,
-          to_name: "JavaScript Mastery",
+          to_name: "Subhava Ojha",
           from_email: form.email,
-          to_email: "sujata@jsmastery.pro",
+          to_email: "dev.subhava@gmail.com",
           message: form.message,
         },
-        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+        'yjLMjud_z9tPFqycJ',
+        
       )
       .then(
         () => {
