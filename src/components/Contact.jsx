beca75380a5e33c004ca5de3143acba0afbecaf0
SUBHAVA OJHA <subhava.22gcebcs190@galgotiacollege.edu>
2025-09-09 23:34:20 +0530

    feat: making Contact section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import { useState, useRef } from "react";
+import emailjs from "@emailjs/browser";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { EarthCanvas } from "./canvas";
+import { SectionWrapper } from "../hoc";
+import { slideIn } from "../utils/motion";
+
+
 
 const Contact = () => {
+  const formRef = useRef();
+  const [Form, setForm] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+  const [loading, setLoading] = useState(false);
+
+  const handleChange = (e) => {}
+
+  const handleSubmit = (e) => {}
+
+
   return (
-    <div>Contact</div>
+    <div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden ">
+      
+    </div>
   )
 }
 
