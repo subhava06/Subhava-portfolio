fbb1bf5b475f0692410cfeeda1ebc4c83ca4868d
SUBHAVA OJHA <subhava.22gcebcs190@galgotiacollege.edu>
2025-09-10 00:13:56 +0530

    feat: EarthCanvas, StarsCanvas

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import { useState, useRef, Suspense} from 'react';
+import { Canvas, useFrame } from '@react-three/fiber';
+import { Points, PointMaterial } from '@react-three/drei';
+import * as random from 'maath/random/dist/maath-random.esm'
 
-const Stars = () => {
+const Stars = (props) => {
   return (
     <div>Stars</div>
   )
 }
 
-export default Stars
+const StarsCanvas = () => {
+
+}
+
+export default StarsCanvas
